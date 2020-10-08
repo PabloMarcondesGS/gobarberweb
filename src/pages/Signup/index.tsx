c2d9f603746f63a9f032ff9e626a5abdb4c5fbad
pablo <pablo@radius.net.br>
2020-10-08 10:48:33 -0300

    adicionando biblioteca unform

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
@@ -9,26 +10,33 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 
-const SigUp: React.FC = () => (
-    <Container>]
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarber" />
+const SigUp: React.FC = () => {
+    function handleSubmint(data: object): void {
+        console.log(data);
+    }
 
-            <form>
-                <h1>Faça seu cadastro</h1>
-                <Input name='name' icon={FiUser} placeholder='Nome' />
-                <Input name='email' icon={FiMail} placeholder='E-mail' />
-                <Input name='password' icon={FiLock} type='password' placeholder='Senha' />
-                <Button type='submit'>Cadastrar</Button>
-            </form>
+    return (
+        <Container>]
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarber" />
 
-            <a href='login'>
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+                {/* initialData={{ name: 'Pablo' }}  usado para carregar um valor padrao no input*/}
+                <Form onSubmit={handleSubmint}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name='name' icon={FiUser} placeholder='Nome' />
+                    <Input name='email' icon={FiMail} placeholder='E-mail' />
+                    <Input name='password' icon={FiLock} type='password' placeholder='Senha' />
+                    <Button type='submit'>Cadastrar</Button>
+                </Form>
+
+                <a href='login'>
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SigUp;
