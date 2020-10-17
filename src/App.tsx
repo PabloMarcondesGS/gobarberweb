bf3b478a32aad859c7b82b507559602b08348111
pablo <pablo@radius.net.br>
2020-10-17 13:08:03 -0300

    separando modulo de contexto

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/Signin';
 import SignUp from './pages/Signup';
 
+import { AuthProvider } from './context/AuthContext';
+
 const App: React.FC = () => (
     <>
-        {/* <SignIn /> */}
-        <SignUp />
+        <AuthProvider>
+            <SignIn />
+            {/* <SignUp /> */}
+        </AuthProvider>
+
         <GlobalStyle />
     </>
 );
